94e49a700c60ac92a56f57cf4659372270b907cf
fransergiomontrel <fransergio.carvalho@montrel.com.br>
2026-08-11 10:59:37 -0300

    SIN AND COS .HEX REMOVED

--- luts_sin.v
+++ luts_sin.v
@@ -4,16 +4,8 @@ input [12:0] addr,
 output reg signed [15:0] sine
 );
 
-reg signed [15:0] sine_lut[0:7215];
+reg signed [15:0] sine_lut[0:3607];
 
-initial begin
-    $readmemh("seno_7215_16bits.hex", sine_lut);
-end
 
-always @(*) begin
-
-    sine = sine_lut[addr];
-
-end
 
 endmodule
